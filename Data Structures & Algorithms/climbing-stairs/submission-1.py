3550ba9b24245d69a70a83b017dad90ecf80953e
Vedant Soni <vedantsoni1207@gmail.com>
2026-06-14 15:22:34 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = {}
+
+        def help(n):
+            if n <= 2:
+                return n
+
+            if n in memo:
+                return memo[n]
+
+            memo[n] = help(n-1) + help(n-2)
+
+            return memo[n]
+
+
+        
+        return help(n)
+        
+        
+
+        
